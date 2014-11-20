94c94ee0139ecdcb17f05728263b89afcf230943
hcbelias <hcbe2004@gmail.com>
2014-11-20 11:26:24 -0800

    Creating a repo

---
+++ Arbitration/Calculation/Arbitration.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Arbitration.Calculation
+{
+    public enum eArbitrationMethod
+    {
+        DistanceToGoal = 0,
+        PercentageToGoal = 1
+    }
+
+    public class Arbitration
+    {
+        #region Public Methods
+
+        /// <summary>
+        /// Creates an array of integers, splitting a number of units among a number of options,
+        /// based on a "Goal"
+        /// </summary>
+        /// <param name="totalNumberOfUnits">Total number of units to split</param>
+        /// <param name="goalValues">The Goals for each item</param>
+        /// <param name="arbitrationMethod">The type of rounding to use</param>
+        /// <param name="initialValues">
+        /// Use this if you already have a starting point and want to arbitrate more units over an
+        /// existing collection.
+        /// </param>
+        /// <param name="limitsOfUnits">
+        /// Limits individually the maximum unit for each element. If the value reaches the limit i
+        /// won't get any more unis, even if has a higher difference to goal.
+        /// </param>
+        /// <returns>
+        /// List with the values distributed as close to the Goal as possible, using the given
+        /// rounding method
+        /// </returns>
+        public IList<int> Arbitrate(int totalNumberOfUnits, ICollection<double> goalValues, eArbitrationMethod arbitrationMethod,
+            IList<int> initialValues = null, IList<int> limitsOfUnits = null)
+        {
+            return (arbitrationMethod == eArbitrationMethod.DistanceToGoal)
+                ? DistanceToGoal(totalNumberOfUnits, goalValues, initialValues, limitsOfUnits)
+                : PercentageToGoal(totalNumberOfUnits, goalValues, initialValues, limitsOfUnits);
+        }
+
+        /// <summary>
+        /// Creates an array of integers, splitting a number of units among a number of options,
+        /// based on a "Goal" using the method "Distance To Goal"
+        /// </summary>
+        /// <param name="totalNumberOfUnits">Total number of units to split</param>
+        /// <param name="goalValues">The Goals for each item</param>
+        /// <param name="initialValues">
+        /// Use this if you already have a starting point and want to arbitrate more units over an
+        /// existing collection.
+        /// </param>
+        /// <param name="limitsOfUnits">
+        /// Limits individually the maximum unit for each element. If the value reaches the limit i
+        /// won't get any more unis, even if has a higher difference to goal.
+        /// </param>
+        /// <returns>
+        /// List with the values distributed as close to the Goal as possible, using the given
+        /// method "Distance To Goal"
+        /// </returns>
+        public IList<int> DistanceToGoal(int totalNumberOfUnits, ICollection<double> goalValues, IList<int> initialValues = null,
+            IList<int> limitsOfUnits = null)
+        {
+            IList<ArbitrationCalc> calcList = CreateStandardList(goalValues, round => round.Goal - round.Value, initialValues);
+            return CalculateArbitration(totalNumberOfUnits, calcList, limitsOfUnits);
+        }
+
+        /// <summary>
+        /// Creates an array of integers, splitting a number of units among a number of options,
+        /// based on a "Goal" using the method "Percentage to Goal"
+        /// </summary>
+        /// <param name="totalNumberOfUnits">Total number of units to split</param>
+        /// <param name="goalValues">The Goals for each item</param>
+        /// <param name="initialValues">
+        /// Use this if you already have a starting point and want to arbitrate more units over an
+        /// existing collection.
+        /// </param>
+        /// <param name="limitsOfUnits">
+        /// Limits individually the maximum unit for each element. If the value reaches the limit i
+        /// won't get any more unis, even if has a higher difference to goal.
+        /// </param>
+        /// <returns>
+        /// List with the values distributed as close to the Goal as possible, using the given
+        /// method "Percentage to Goal"
+        /// </returns>
+        public IList<int> PercentageToGoal(int totalNumberOfUnits, ICollection<double> goalValues, IList<int> initialValues = null,
+            IList<int> limitsOfUnits = null)
+        {
+            IList<ArbitrationCalc> calcList = CreateStandardList(goalValues, round => 1 - (round.Value / round.Goal), initialValues);
+            return CalculateArbitration(totalNumberOfUnits, calcList, limitsOfUnits);
+        }
+
+        #endregion Public Methods
+
+        #region Private Methods
+
+        private IList<int> CalculateArbitration(int totalNumberOfUnits, IList<ArbitrationCalc> calcList, IList<int> LimitsOfUnits = null)
+        {
+            //If the total number of units is higher than the sum of the limits of units, throws an exception, since it won't be possible to
+            //split the total among the possibilities available
+            if ((LimitsOfUnits != null) && (totalNumberOfUnits > LimitsOfUnits.Sum(s => s)))
+                throw new ArgumentOutOfRangeException("totalNumberOfUnits");
+
+            //Adds units to each value in the collection based on the highest difference to the Goal.
+            //The Difference is calculated based on the rounding method provided
+            //If it was set the limits of units, only increase quantity if the limits is not reached yet, no matter the difference
+            for (int aux = 0; aux < totalNumberOfUnits; aux++)
+            {
+                ArbitrationCalc item = calcList
+                    .Select((v, i) => new { Item = v, Index = i })
+                    .Where(v => (LimitsOfUnits == null) || (v.Item.Value < LimitsOfUnits[v.Index]))
+                    .OrderByDescending(r => r.Item.Difference)
+                    .ThenByDescending(r => r.Item.Goal)
+                    .Select(v => v.Item)
+                    .FirstOrDefault();
+
+                item.Value = item.Value + 1;
+            }
+
+            return calcList.Select(c => c.Value).ToList();
+        }
+
+        private IList<ArbitrationCalc> CreateStandardList(ICollection<double> goalValues, Func<ArbitrationCalc, double> calculationMethod,
+            IList<int> initialValues = null)
+        {
+            return goalValues.Select((d, i) =>
+                new ArbitrationCalc() { Goal = d, CalculationMethod = calculationMethod, Value = (initialValues == null) ? 0 : initialValues[i] }).ToList();
+        }
+
+        #endregion Private Methods
+
+    }
+
+    public class ArbitrationCalc
+    {
+        #region Public Properties
+
+        public Func<ArbitrationCalc, double> CalculationMethod { get; set; }
+
+        public double Difference { get { return CalculationMethod(this); } }
+
+        public double Goal { get; set; }
+
+        public int Value { get; set; }
+
+        #endregion Public Properties
+    }
+}
---
+++ Arbitration/MainWindow.xaml.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Arbitration.Calculation;
+
+namespace Arbitration
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.TotalUnitsInput.Focus();
+        }
+
+
+        private void AddNewValueButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(this.AddNewValueInput.Text))
+            {
+                if (IsValidInput(this.AddNewValueInput.Text, false))
+                {
+                    this.ArbitrationGrid.Items.Add(new GridModel()
+                    {
+                        GoalNumberOfUnits = Double.Parse(this.AddNewValueInput.Text),
+                        ArbitratedNumberOfUnits = 0
+                    });
+                    UpdateArbitration();
+                    this.AddNewValueInput.Focus();
+                    this.AddNewValueInput.SelectAll();
+                }
+                else
+                {
+                    ShowInvalidValueErrorMessage(this.AddNewValueInput.Text);
+                }
+            }
+        }
+
+        private bool IsValidInput(string input, bool isInt)
+        {
+            Match match = isInt ? Regex.Match(input, @"^[0-9]*$") : Regex.Match(input, @"^[0-9]*(?:\.[0-9]*)?$");
+
+            if (match.Success)
+            {
+                bool parsedSuccess = false;
+                double parsedValue = 0;
+                parsedSuccess = Double.TryParse(input, out parsedValue);
+                return parsedSuccess;
+            }
+            return false;
+        }
+
+        private void RemoveValue_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.ArbitrationGrid.SelectedItems.Count > 0)
+            {
+                IEnumerator enumerator = this.ArbitrationGrid.SelectedItems.GetEnumerator();
+                List<GridModel> itemsToBeRemoved = new List<GridModel>();
+                while (enumerator.MoveNext())
+                {
+                    GridModel iterator = (GridModel)enumerator.Current;
+                    itemsToBeRemoved.Add(iterator);
+                }
+                itemsToBeRemoved.ForEach(p =>
+                {
+                    this.ArbitrationGrid.Items.Remove(p);
+                });
+                UpdateArbitration();
+            }
+        }
+
+        private void AddNewValueInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            string text = textBox.Text;
+            if (!string.IsNullOrWhiteSpace(text))
+            {
+                if (!IsValidInput(text, false))
+                {
+                    ShowInvalidValueErrorMessage(text.Substring(text.Count() - 1));
+                    textBox.Text = text.Substring(0, text.Count() - 1);
+                }
+            }
+        }
+
+        private void ShowInvalidValueErrorMessage(string text)
+        {
+            ShowErrorMessage(string.Format("Invalid Value('{0}').", text));
+        }
+
+
+
+        private void ShowErrorMessage(string text)
+        {
+            MessageBox.Show(text);
+        }
+
+        private void UpdateArbitration()
+        {
+            if (this.TotalUnitsInput != null && !string.IsNullOrWhiteSpace(this.TotalUnitsInput.Text))
+            {
+                List<double> goalValues = GetGoalValues();
+                if (goalValues.Any())
+                {
+                    int totalUnits = Int32.Parse(this.TotalUnitsInput.Text);
+                    eArbitrationMethod method = (eArbitrationMethod)this.MethodCombobox.SelectedIndex;
+                    Arbitration.Calculation.Arbitration calculator = new Arbitration.Calculation.Arbitration();
+                    IList<int> arbitrateValues = calculator.Arbitrate(totalUnits, goalValues, method);
+                    SetArbitrateValues(arbitrateValues);
+                }
+            }
+        }
+
+        private List<double> GetGoalValues()
+        {
+            return GetGridData().Select(p => p.GoalNumberOfUnits).ToList();
+        }
+
+        private List<GridModel> GetGridData()
+        {
+            List<GridModel> returnObj = new List<GridModel>();
+            IEnumerable enumerable = this.ArbitrationGrid.Items.SourceCollection;
+            IEnumerator enumerator = enumerable.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                GridModel iterator = (GridModel)enumerator.Current;
+                returnObj.Add(iterator);
+            }
+            return returnObj.ToList();
+        }
+
+
+        private void SetArbitrateValues(IList<int> arbitrateValues)
+        {
+            List<GridModel> returnObj = GetGridData();
+
+            for (int i = 0; i < returnObj.Count; i++)
+            {
+                returnObj.ElementAt(i).ArbitratedNumberOfUnits = arbitrateValues[i];
+            }
+
+            this.ArbitrationGrid.Items.Refresh();
+        }
+
+        private void MethodCombobox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateArbitration();
+        }
+
+        private void TotalUnitsInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            string text = textBox.Text;
+            if (!string.IsNullOrWhiteSpace(text))
+            {
+                if (!IsValidInput(text, true))
+                {
+                    ShowInvalidValueErrorMessage(text.Substring(text.Count() - 1));
+                    textBox.Text = text.Substring(0, text.Count() - 1);
+                }
+                else
+                {
+                    UpdateArbitration();
+                }
+            }
+        }
+
+        private void PasteUnitsButton_Click(object sender, RoutedEventArgs e)
+        {
+            object contentCopy = Clipboard.GetData("Text");
+            List<GridModel> oldGridData = this.GetGridData();
+            if (contentCopy != null)
+            {
+                try
+                {
+                    string contentString = (string)contentCopy;
+
+                    foreach (string row in contentString.Split('\n'))
+                    {
+                        if (!string.IsNullOrEmpty(row))
+                        {
+                            List<string> cellList = row.Split('\r', '\t').ToList();
+                            cellList.RemoveAll(p => string.IsNullOrWhiteSpace(p));
+
+                            switch (cellList.Count)
+                            {
+                                case 0:
+                                    this.ArbitrationGrid.Items.Add(new GridModel()
+                                    {
+                                        CurrentNumberOfUnits = 0,
+                                        GoalNumberOfUnits = 0,
+                                        ArbitratedNumberOfUnits = 0
+                                    });
+                                    break;
+                                case 1:
+                                    this.ArbitrationGrid.Items.Add(new GridModel()
+                                    {
+                                        CurrentNumberOfUnits = 0,
+                                        GoalNumberOfUnits = Double.Parse(cellList[0]),
+                                        ArbitratedNumberOfUnits = 0
+                                    });
+                                    break;
+                                default:
+                                    this.ArbitrationGrid.Items.Add(new GridModel()
+                                    {
+                                        CurrentNumberOfUnits = Double.Parse(cellList[0]),
+                                        GoalNumberOfUnits = Double.Parse(cellList[1]),
+                                        ArbitratedNumberOfUnits = 0
+                                    });
+                                    break;
+                            }
+                        }
+                    }
+                    
+                }
+                catch (Exception error)
+                {
+                    this.ArbitrationGrid.Items.Clear();
+                    oldGridData.ForEach(p => {
+                        this.ArbitrationGrid.Items.Add(p);
+                    });
+                    ShowErrorMessage(error.Message);
+                }
+                finally
+                {
+                    UpdateArbitration();
+                }
+            }
+        }
+
+
+    }
+
+    public class GridModel
+    {
+        public double GoalNumberOfUnits { get; set; }
+        public double CurrentNumberOfUnits { get; set; }
+        public int ArbitratedNumberOfUnits { get; set; }
+    }
+
+
+}
